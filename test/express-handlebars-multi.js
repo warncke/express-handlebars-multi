265cf0fecaddd5f117737ca98ca798d8cfa933e0
Ersun Warncke <ersun.warncke@gmail.com>
2017-01-31 16:08:52 -0400

    initial working version

--- test/express-handlebars-multi.js
+++ test/express-handlebars-multi.js
@@ -20,4 +20,114 @@ describe('express-handlebars-multi', function () {
         var engine = ExpressHandlebarsMulti()
     })
 
+    it('should load multiple partials and use first', function () {
+        // create new engine
+        var engine = ExpressHandlebarsMulti({
+            layoutDirs: [__dirname+'/'+'bar/layouts', __dirname+'/'+'foo/layouts'],
+            partialDirs: [__dirname+'/'+'foo/partials', __dirname+'/'+'bar/partials'],
+        })
+        // render template with engine
+        return engine(__dirname+'/foo/views/foo.view.hbs', {})
+        // check result
+        .then(data => {
+            assert.strictEqual(data, 'foo/foo/partial')
+        })
+    })
+
+    it('should load multiple layouts and use first', function () {
+        // create new engine
+        var engine = ExpressHandlebarsMulti({
+            layoutDirs: [__dirname+'/'+'bar/layouts', __dirname+'/'+'foo/layouts'],
+            partialDirs: [__dirname+'/'+'foo/partials', __dirname+'/'+'bar/partials'],
+        })
+        // render template with engine
+        return engine(__dirname+'/foo/views/foo.view.hbs', {
+            layout: 'foo.layout',
+        })
+        // check result
+        .then(data => {
+            assert.strictEqual(data, 'bar/foo: foo/foo/partial')
+        })
+    })
+
+    it('should use default layout', function () {
+        // create new engine
+        var engine = ExpressHandlebarsMulti({
+            defaultLayout: 'foo.layout',
+            layoutDirs: [__dirname+'/'+'bar/layouts', __dirname+'/'+'foo/layouts'],
+            partialDirs: [__dirname+'/'+'foo/partials', __dirname+'/'+'bar/partials'],
+        })
+        // render template with engine
+        return engine(__dirname+'/foo/views/foo.view.hbs', {})
+        // check result
+        .then(data => {
+            assert.strictEqual(data, 'bar/foo: foo/foo/partial')
+        })
+    })
+
+    it('should render layout with html in template', function () {
+        // create new engine
+        var engine = ExpressHandlebarsMulti({
+            defaultLayout: 'foo.layout',
+            layoutDirs: [__dirname+'/'+'bar/layouts', __dirname+'/'+'foo/layouts'],
+            partialDirs: [__dirname+'/'+'bar/partials', __dirname+'/'+'foo/partials'],
+        })
+        // render template with engine
+        return engine(__dirname+'/foo/views/foo.view.hbs', {})
+        // check result
+        .then(data => {
+            assert.strictEqual(data, 'bar/foo: <h1>bar/foo/partial</h1>')
+        })
+    })
+
+    it('should render template with params', function () {
+        // create new engine
+        var engine = ExpressHandlebarsMulti({
+            defaultLayout: 'foo.layout',
+            layoutDirs: [__dirname+'/'+'bar/layouts', __dirname+'/'+'foo/layouts'],
+            partialDirs: [__dirname+'/'+'bar/partials', __dirname+'/'+'foo/partials'],
+        })
+        // render template with engine
+        return engine(__dirname+'/bar/views/bar.view.hbs', {foo: 'bar'})
+        // check result
+        .then(data => {
+            assert.strictEqual(data, 'bar/foo: bar/bar/partial bar')
+        })
+    })
+
+    it('should set config', function () {
+        // create new engine
+        var engine = ExpressHandlebarsMulti({
+            layoutDirs: [__dirname+'/'+'bar/layouts', __dirname+'/'+'foo/layouts'],
+            partialDirs: [__dirname+'/'+'bar/partials', __dirname+'/'+'foo/partials'],
+        })
+        // set/get config
+        ExpressHandlebarsMulti.config({defaultLayout: 'foo.layout'})
+        // render template with engine
+        return engine(__dirname+'/bar/views/bar.view.hbs', {foo: 'bar'})
+        // check result
+        .then(data => {
+            assert.strictEqual(data, 'bar/foo: bar/bar/partial bar')
+        })
+    })
+
+    it('should use helpers', function () {
+        // create new engine
+        var engine = ExpressHandlebarsMulti({
+            helpers: {
+                fooHelper: function (val) {
+                    return 'foo'+val
+                }
+            },
+        })
+        // set/get config
+        ExpressHandlebarsMulti.config({defaultLayout: 'foo.layout'})
+        // render template with engine
+        return engine(__dirname+'/foo/views/helper.view.hbs', {})
+        // check result
+        .then(data => {
+            assert.strictEqual(data, 'footest')
+        })
+    })
+
 })
